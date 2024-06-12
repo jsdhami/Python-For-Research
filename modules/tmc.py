112ee994fa3a9012c05464d1e24bfc358ef5b748
Janak Singh Dhami <139155991+jsdhami@users.noreply.github.com>
2024-06-12 16:49:44 +0000

    add

---
+++ modules/tmc.py
@@ -0,0 +1,6 @@
+# this is out tmc module
+# create function "add"
+def add(a,b):
+      return a+b
+
+add(6,5)
---
+++ modules/use.py
@@ -0,0 +1,9 @@
+import tmc
+# or
+import tmc as t
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+sum = t.add(6,5)
+
+print("Sum is: ", sum)
